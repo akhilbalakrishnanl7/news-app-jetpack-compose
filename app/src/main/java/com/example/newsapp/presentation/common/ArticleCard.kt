bd82cab49419acff2925e45158874fd43dd08245
Akhil Balakrishnan <akhil.b@litmus7.com>
2023-12-31 14:08:17 +0530

    1. Api for getting news from "everything" endpoint is created. 2. Implemented paging to fetch data from server by using Paging3 library 3. Created ArticleCard 4. Created ShimmerEffect animation

---
+++ app/src/main/java/com/example/newsapp/presentation/common/ArticleCard.kt
@@ -0,0 +1,111 @@
+package com.example.newsapp.presentation.common
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.example.newsapp.R
+import com.example.newsapp.domain.model.Article
+import com.example.newsapp.domain.model.Source
+import com.example.newsapp.presentation.Dimens.ArticleCardSize
+import com.example.newsapp.presentation.Dimens.ExtraSmallPadding
+import com.example.newsapp.presentation.Dimens.ExtraSmallPadding2
+import com.example.newsapp.presentation.Dimens.SmallIconSize
+import com.example.newsapp.ui.theme.NewsAppTheme
+
+@Composable
+fun ArticleCard(
+    modifier: Modifier = Modifier,
+    article: Article,
+    onClick: () -> Unit
+) {
+    val context = LocalContext.current
+
+    Row(modifier = modifier.clickable { onClick() }) {
+        AsyncImage(
+            modifier = Modifier
+                .size(ArticleCardSize)
+                .clip(MaterialTheme.shapes.medium),
+            model = ImageRequest.Builder(context = context).data(article.urlToImage).build(),
+            contentDescription = null
+        )
+        Column(
+            verticalArrangement = Arrangement.SpaceAround,
+            modifier = Modifier
+                .padding(horizontal = ExtraSmallPadding)
+                .height(ArticleCardSize)
+        ) {
+            Text(
+                text = article.title,
+                style = MaterialTheme.typography.bodyMedium,
+                color = colorResource(id = R.color.text_title),
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text(
+                    text = article.source.name,
+                    style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.Bold),
+                    color = colorResource(id = R.color.body)
+                )
+                Spacer(modifier = Modifier.width(ExtraSmallPadding2))
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_time),
+                    contentDescription = null,
+                    modifier = Modifier.size(SmallIconSize),
+                    tint = colorResource(id = R.color.body)
+                )
+                Spacer(modifier = Modifier.width(ExtraSmallPadding2))
+                Text(
+                    text = article.publishedAt,
+                    style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.Bold),
+                    color = colorResource(id = R.color.body)
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Preview(uiMode = UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun ArticleCardPreview() {
+    NewsAppTheme {
+        Surface {
+            ArticleCard(
+                article = Article(
+                    author = "Julian Lin",
+                    content = "KE ZHUANG\r\nIs there a more beaten-down growth stock than SolarEdge (SEDG)? 2023 saw tech stocks across the board recover much of their losses suffered in 2022. SEDG appears to be experiencing a delay… [+9106 chars]",
+                    description = "SolarEdge faces challenges but remains optimistic with a strong cash balance sheet and attractive valuation. Check out the full analysis of SEDG stock.",
+                    publishedAt = "2024-01-01T09:15:34Z",
+                    source = Source(id = "", name = "BBC"),
+                    title = "SolarEdge: I Was So Wrong, But This Is Too Much",
+                    url = "https://seekingalpha.com/article/4660590-solaredge-i-was-so-wrong-but-this-is-too-much",
+                    urlToImage = "https://static.seekingalpha.com/cdn/s3/uploads/getty_images/1468753570/image_1468753570.jpg?io=getty-c-w1536"
+                )
+            ) {}
+        }
+    }
+}
