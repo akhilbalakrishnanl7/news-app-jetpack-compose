68ab0cc5bcdef3a7220a4ba4cdeee1444013807e
Akhil Balakrishnan <akhil.b@litmus7.com>
2024-01-01 18:40:05 +0530

    1. Home screen created and displayed the articles.

--- app/src/main/java/com/example/newsapp/presentation/navgraph/NavGraph.kt
+++ app/src/main/java/com/example/newsapp/presentation/navgraph/NavGraph.kt
@@ -1,12 +1,14 @@
 package com.example.newsapp.presentation.navgraph
 
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.example.newsapp.presentation.home.HomeScreen
+import com.example.newsapp.presentation.home.HomeViewModel
 import com.example.newsapp.presentation.onboarding.OnBoardingScreen
 import com.example.newsapp.presentation.onboarding.OnBoardingViewModel
 
@@ -38,7 +40,9 @@ fun NavGraph(
             composable(
                 route = Route.NewsNavigatorScreen.route
             ) {
-                Text(text = "News navigator screen")
+                val viewModel:HomeViewModel = hiltViewModel()
+                val articles = viewModel.news.collectAsLazyPagingItems()
+                HomeScreen(articles = articles, navigate = {})
             }
         }
     }
